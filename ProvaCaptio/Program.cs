73cf42320cabede8976eee34e6daff604723daa6
Rodrigo Obalat Azcárate <Rodrigo.obalat@gmail.com>
2021-08-09 07:34:18 +0200

    Init

---
+++ ProvaCaptio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProvaCaptio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
