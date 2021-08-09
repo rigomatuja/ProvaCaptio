e5ac601e604524bc1257ab78241718360fec3d7d
Rodrigo Obalat Azcárate <Rodrigo.obalat@gmail.com>
2021-08-09 12:43:58 +0200

    UI FIX

--- ProvaCaptio/Program.cs
+++ ProvaCaptio/Program.cs
@@ -6,11 +6,14 @@ namespace ProvaCaptio
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Password Validator");
+            Console.WriteLine("\nTechnical test - Password Validator");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("The Password must satisfy the next requirements:");
             Console.WriteLine("At least one uppercase character is required.");
             Console.WriteLine("At least one lowercase character is required.");
             Console.WriteLine("At least one number is required.");
             Console.WriteLine("A minimum length of 8 characters is required.");
+            Console.WriteLine("---------------------------------------------");
             Console.WriteLine("\nType a password:");
             var validator = new ValidatePassword(Console.ReadLine());
             while (!validator.Validate())
